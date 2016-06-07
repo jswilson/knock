7059f054907ccbd19de2b4e1feb08ac97e4db96d
Jonathan Wilson <js.wilson@ymail.com>
2016-06-07 16:20:10 -0600

    Change request header where we search for the token

--- lib/knock/authenticable.rb
+++ lib/knock/authenticable.rb
@@ -1,7 +1,7 @@
 module Knock::Authenticable
   def current_user
     @current_user ||= begin
-      token = params[:token] || request.headers['Authorization'].split.last
+      token = params[:token] || request.headers['HTTP_AUTHENTICATION'].split.last
       Knock::AuthToken.new(token: token).current_user
     rescue
       nil
